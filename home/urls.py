bc97f751d229b05d21671a49f27faf9f1162de16
Lite Li <litekok@gmail.com>
2011-03-12 11:44:49 +0800

    add sitemap

---
+++ home/urls.py
@@ -0,0 +1,32 @@
+from django.conf.urls.defaults import *
+from django.contrib.syndication.feeds import Feed
+from django.contrib.sitemaps import FlatPageSitemap, GenericSitemap
+from djproj.blog.models import Entry
+
+class LatestEntries(Feed):
+    title = "My Blog"
+    link = "/archive/"
+    description = "The latest news about stuff."
+
+    def items(self):
+        return Entry.objects.order_by('-pub_date')[:5]
+
+info_dict = {
+    'queryset': Entry.objects.all(),
+    'date_field': 'pub_date',
+}
+
+sitemaps = {
+    'flatpages': FlatPageSitemap,
+    'blog': GenericSitemap(info_dict, priority=0.6),
+}
+
+feeds = {
+    'latest': LatestEntries,
+    # 'categories': LatestEntriesByCategory,
+}
+
+urlpatterns = patterns('',
+    (r'feeds/(?P<url>.*)/$', 'django.contrib.syndication.views.feed', {'feed_dict': feeds}),
+    (r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': sitemaps})
+)
